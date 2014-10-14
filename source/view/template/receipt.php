4ed393b326abfb2085bf0a4097ce01cdfe930e2b
Goutam <goutamnair7@gmail.com>
2014-10-15 00:08:53 +0530

    Added FPDF to convert HTML to PDF R=Goutam

---
+++ source/view/template/receipt.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML>
+<html>
+<link href="../../asset/css/bootstrap/bootstrap.css" rel="stylesheet"/>
+<body>
+    <a href="../../controller/receipt.php">Convert to PDF</a>
+    <div class="container" style="padding-top:2em;padding-left:15em;padding-right:12em;">
+        <div class="col-xs-12">
+            <div class="col-xs-6">
+                <img src="../../asset/img/cie-image.jpg" width="148px" height="148px"></img>
+            </div>
+            <div class="col-xs-6">
+                <p style="margin-top:3em;font-size:1.4em;"><b>BANYAN INTELLECTUAL INITIATIVES</b></p><br>
+                <p style="margin-top:-1em;font-size:1.2em;">(The IIIT-H Foundation)</p>
+            </div>
+        </div>
+        <div class="col-xs-12" style="margin-top:3em;font-size:1.3em;">
+            <center><p><b><u>Receipt</u></b></p></center>
+        </div>
+        <div class="col-xs-12" style="margin-top:1em;">
+            <div class="col-xs-6">
+                <p><b>To:</b></p>
+                <p>Name, Designation</p>
+                <p>Company, </p>
+                <p>Address</p>
+            </div>
+            <div class="col-xs-6">
+                <p style="float:right;"><b>Date:</b>31st, Feb 2014</p>
+            </div>
+        </div>
+        <div class="col-xs-12" style="margin-top:2em;">
+            Received from John the sum of Rs. Three Thousand Two Hundred and Fifty, which is the rent for using the Events Room from 6:30 PM to 8:30 PM.
+        </div>
+        <div class="col-xs-12" style="margin-top:3em;">
+            <p>Regards, </p>
+        </div>
+        <div class="col-xs-12" style="margin-top:2em;">
+            <p>Raghu Prodduturi</p>
+            <p>Manager</p>
+            <p>Banyan Intellectual Initiatives</p>
+        </div>
+    </div>
+</body>
+</html>
