87f8d120062b156908129b6a7fb6354ab476300c
Saksham Aggarwal <s.agg@live.com>
2014-11-04 15:00:13 +0530

    Don't Know

--- source/model/config_sql.php
+++ source/model/config_sql.php
@@ -1,12 +1,12 @@
 <?php
-	
-	$sql_hostname = "localhost"; //Enter hostname
-	$sql_user = "goutam"; // Enter Username
-	$sql_password = "1234"; // Enter password
-	$sql_db = "cie"; // Enter db_name
-	
-	$mysqli = new mysqli( $sql_hostname, $sql_user, $sql_password, $sql_db );
-	if ($mysqli == 0) {
-		die("Connection Problem. Check credentials.");
-	}
+
+$sql_hostname = "localhost"; //Enter hostname
+$sql_user = "root"; // Enter Username
+$sql_password = "root"; // Enter password
+$sql_db = "ssad_production"; // Enter db_name
+
+$mysqli = new mysqli( $sql_hostname, $sql_user, $sql_password, $sql_db );
+if ($mysqli == 0) {
+	die("Connection Problem. Check credentials.");
+}
 ?>
