12f49f425e95239cdb6478282dfa27127f5ca7fd
Gaurav Mittal <gaurav.mittal@students.iiit.ac.in>
2014-11-05 06:38:18 +0530

    Added Multiple Functionalities in Invoice Module R=gauravmittal1995

--- source/model/config_sql.php
+++ source/model/config_sql.php
@@ -1,5 +1,5 @@
 <?php
-<<<<<<< HEAD
+
 
 $sql_hostname = "localhost"; //Enter hostname
 $sql_user = "root"; // Enter Username
@@ -10,16 +10,4 @@ $mysqli = new mysqli( $sql_hostname, $sql_user, $sql_password, $sql_db );
 if ($mysqli == 0) {
 	die("Connection Problem. Check credentials.");
 }
-=======
-	
-	$sql_hostname = "localhost"; //Enter hostname
-	$sql_user = "root"; // Enter Username
-	$sql_password = "root"; // Enter password
-	$sql_db = "ssad_production"; // Enter db_name
-	
-	$mysqli = new mysqli( $sql_hostname, $sql_user, $sql_password, $sql_db );
-	if ($mysqli == 0) {
-		die("Connection Problem. Check credentials.");
-	}
->>>>>>> 3c32e8bbe0b2ed87d0600f9470eae9248ab40174
 ?>
--- source/view/receipt.php
+++
@@ -1,4 +0,0 @@
-<html>
-<p>A test file for generating receipt.</p>
-<button onclick="generatePDF">Convert to pdf</button>
-</html>
