d8784f1e8a614b6f7b4fd2d5db06a1054cb18513
Anurag <gupta.anu1995@gmail.com>
2014-11-15 15:09:25 +0530

    UI changes + ensure login

--- source/view/receipt_form.php
+++ source/view/receipt_form.php
@@ -166,11 +166,8 @@
 				</div>
 			</div>
 			
-			<footer id="footer-bar" class="row">
-				<p id="footer-copyright" class="col-xs-12">
-				&copy; 2014 <a href="http://www.adbee.sk/" target="_blank">Adbee digital</a>. Powered by Centaurus Theme.
-				</p>
-			</footer>
+			<!--<footer id="footer-bar" class="row">
+			</footer> -->
 		</div>
 	</div>
 </div>
