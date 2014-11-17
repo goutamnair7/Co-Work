437685eaaa841135304fc4bddc67b817276bcac2
Goutam <goutamnair7@gmail.com>
2014-11-18 02:12:53 +0530

    Show Previous Invoices with ability to Confirm and Pending done

--- source/controller/change_status.php
+++ source/controller/change_status.php
@@ -7,8 +7,13 @@
 require( dirname( dirname( __FILE__ ) ) . DIRECTORY_SEPARATOR . "model" . DIRECTORY_SEPARATOR . "config_sql.php" );
 
 $id = $_GET['id'];
+$action = $_GET['action'];
 
-$change_query = "UPDATE invoice SET status=1 WHERE id=$id";
+if($action == "confirm")
+    $change_query = "UPDATE invoice SET status=1 WHERE id=$id";
+else if($action == "pending")
+    $change_query = "UPDATE invoice SET status=0 WHERE id=$id";
+
 $result_change = $mysqli->query( $change_query );
 
 if($result_change) {
@@ -17,7 +22,7 @@ if($result_change) {
 	 * Redirect to the page from where its called. Also can use ajax for best use.
 	 * If ajax is used instead of header(LOCATION), use echo "Invoice Completed."
 	 */
-	header("LOCATION: ../view/invoice.php");
+	echo "Success";
 
 } else {
 
--- source/model/config_sql.php
+++ source/model/config_sql.php
@@ -2,9 +2,9 @@
 
 
 $sql_hostname = "localhost"; //Enter hostname
-$sql_user = "root"; // Enter Username
-$sql_password = "root"; // Enter password
-$sql_db = "ssad_production"; // Enter db_name
+$sql_user = "goutam"; // Enter Username
+$sql_password = "1234"; // Enter password
+$sql_db = "cie"; // Enter db_name
 
 $mysqli = new mysqli( $sql_hostname, $sql_user, $sql_password, $sql_db );
 if ($mysqli == 0) {
